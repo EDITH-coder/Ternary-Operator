499f33037eb7c1b4dfe97addb4498543c48a8c2a
EDITH_Coder <edithmashapo@gmail.com>
2023-10-16 04:36:32 +0200

    Add project files.

---
+++ Ternary Operator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 20, secondNumber = 10;
+
+            var result = firstNumber > secondNumber? "firstNumber is greater than secondNumber" : "firstNumber is less than secondNumber";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
